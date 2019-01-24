51b6ee4c49703df0adbb44c0d37d1ebb247e82b0
JoshuaPrice <jprice@berkeley.edu>
2019-01-24 10:57:28 -0800

    deleted gffFuncs.py

--- gffFuncs.py
+++
@@ -1,8 +0,0 @@
-import pprint
-from BCBio.GFF import GFFExaminer
-
-in_file = "mus_musculus.GRCm38.Regulatory_Build.regulatory_features.20161111.gff"
-examiner = GFFExaminer()
-in_handle = open(in_file)
-pprint.pprint(examiner.parent_child_map(in_handle))
-in_handle.close()
